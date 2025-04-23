4bf64cbc1f382928f91ee08718c767f075314486
Yash Rathi <yarathi1999@gmail.com>
2025-04-23 22:12:43 +0530

    Updated the file structure and added flask to implement backend

---
+++ micro-skill-exchange/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
